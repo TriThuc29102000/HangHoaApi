8912fc2120a8c9998da21dbf9cfdd75cedb8b913
TriThuc29102000 <ntrithuc35@gmail.com>
2022-06-13 08:47:37 +0700

    Revert "gggggjh"
    This reverts commit 8883216903b170be0e2cf1f9666570a20086eed2.
    
    # Conflicts:
    #	HangHoaApi/Startup.cs

--- HangHoaApi/Program.cs
+++ HangHoaApi/Program.cs
@@ -11,6 +11,20 @@ namespace HangHoaApi
 {
     public class Program
     {
+        /*
+         Host (IHost) object:
+            - Dependency Injection (ID) : IServiceProvider (ServiceCollertion)
+            - Logging (ILogging)
+            - Configiraton
+            - IHostedService => StarAsync : Run HTTP Server (Kestrel Http)
+
+        1) T?o IHostBuilder
+        2) Cau hinh, dang ki cac dich vu ( goi ConfigureWebHostDefaults)
+        3) IHostBuilder.Build() => Host( IHost)
+        4) Host.Run()
+
+        Request => pipeline(Middleware)
+        */
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
